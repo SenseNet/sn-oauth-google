306629b8a7aca7697c30ff2db996c21e2c6ab2f8
tusmester <tusmester@gmail.com>
2017-11-27 15:42:44 +0100

    [KFI] Google OAuth provider initial commit.

---
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -0,0 +1,16 @@
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
+$installPackageFolder = "$srcPath\nuget\content\Admin\tools"
+$installPackagePath = "$installPackageFolder\install-oauth-google.zip"
+
+# delete existing packages
+Remove-Item $PSScriptRoot\*.nupkg
+
+if (!(Test-Path $installPackageFolder))
+{
+	New-Item $installPackageFolder -Force -ItemType Directory
+}
+
+Compress-Archive -Path "$srcPath\nuget\snadmin\install-oauth-google\*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePath
+
+nuget pack $srcPath\SenseNet.OAuth.Google\SenseNet.OAuth.Google.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+nuget pack $srcPath\SenseNet.OAuth.Google\SenseNet.OAuth.Google.Install.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
