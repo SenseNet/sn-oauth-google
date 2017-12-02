760d6c57b307d19396bb6f00b309c9f02df41b12
tusmester <tusmester@gmail.com>
2017-12-02 20:25:46 +0100

    [KFI] Raise Services dependency version to 7.0 stable and change current version to 1.0.

--- src/SenseNet.OAuth.Google/Properties/AssemblyInfo.cs
+++ src/SenseNet.OAuth.Google/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0")]
-[assembly: AssemblyInformationalVersion("7.0.0-beta0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("94bd999e-119f-45bd-a9c2-e542d7dbae9a")]
